63619412e85735f9d5337b912b773c6baa575c30
Christian <christian.fikus@wp.pl>
2023-12-04 16:40:19 +0100

    Day 4

--- Advent-Of-Code-2023/Days/Day4.cpp
+++ Advent-Of-Code-2023/Days/Day4.cpp
@@ -1,13 +1,92 @@
 #include "../general.h"
+#include <vector>
+#include <set>
+#include <map>
 
-string Day4_Part1(stringstream& input)
+int Day4_Part1(stringstream& input)
 {
-	return "Not implemented yet";
+	int sum = 0;
+	while (!input.eof())
+	{
+		string line;
+		getline(input, line);
+		stringstream ssline(line);
+		ssline >> line >> line;
+
+		string num;
+		bool firstPart = true;
+		set<int> winnings;
+		int n = 0;
+
+		while (!ssline.eof())
+		{
+			ssline >> num;
+			if (firstPart)
+			{
+				if (num == "|")
+				{
+					firstPart = false;
+					continue;
+				}
+				winnings.insert(stoi(num));
+			}
+			else
+			{
+				if (winnings.contains(stoi(num)))
+					n++;
+			}
+		}
+		if (n > 0)
+			sum += pow(2, n - 1);
+	}
+
+	return sum;
 }
 
-string Day4_Part2(stringstream& input)
+int Day4_Part2(stringstream& input)
 {
-	return "Not implemented yet";
+	int cards = 0;
+	map<int, int> Cards;
+
+	while (!input.eof())
+	{
+		int cartNumber;
+		string line;
+		getline(input, line);
+		stringstream ssline(line);
+		ssline >> line >> cartNumber >> line;
+
+		Cards[cartNumber]++; //Original
+
+		string num;
+		bool firstPart = true;
+		set<int> winnings;
+
+		int cnum = cartNumber;
+		while (!ssline.eof())
+		{
+			ssline >> num;
+			if (firstPart)
+			{
+				if (num == "|")
+				{
+					firstPart = false;
+					continue;
+				}
+				winnings.insert(stoi(num));
+			}
+			else
+			{
+				if (winnings.contains(stoi(num)))
+				{
+					Cards[++cnum] += Cards[cartNumber]; //Copies
+				}
+			}
+		}
+		cards += Cards[cartNumber];
+	}
+
+	return cards;
 }
 
 
