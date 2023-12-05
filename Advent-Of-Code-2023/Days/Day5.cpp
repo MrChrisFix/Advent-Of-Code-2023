18fff58ce95ca4952726241bcd8531df1fe70ba0
Christian Fikus <christian.fikus@wp.pl>
2023-12-05 16:10:17 +0100

    Day 5

--- Advent-Of-Code-2023/Days/Day5.cpp
+++ Advent-Of-Code-2023/Days/Day5.cpp
@@ -1,13 +1,162 @@
 #include "../general.h"
+#include <map>
+#include <set>
+#include <vector>
 
-string Day5_Part1(stringstream& input)
+#define ll long long
+
+struct Mapping
 {
-	return "Not implemented yet";
+	ll dest, source, range;
+	bool exists(ll src)
+	{
+		return src >= source && src < source + range;
+	}
+	ll getNum(ll src)
+	{
+		ll offset = src - source;
+		return dest + offset;
+	}
+};
+
+struct Seed
+{
+	ll start, range;
+	bool isValid(ll seed)
+	{
+		return seed >= start && seed < start + range;
+	}
+};
+
+bool isValidSeed(ll seed, vector<Seed> seeds)
+{
+	for (int i = 0; i < seeds.size(); i++)
+	{
+		if (seeds[i].isValid(seed))
+			return true;
+	}
+	return false;
 }
 
-string Day5_Part2(stringstream& input)
+ll FindNum(vector<Mapping> map, ll num)
 {
-	return "Not implemented yet";
+	for (int i = 0; i < map.size(); i++)
+	{
+		if (map[i].exists(num))
+			return map[i].getNum(num);
+	}
+	return num;
+}
+
+ll Day5_Part1(stringstream& input)
+{
+	vector<Mapping> seedToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemp, tempToHum, humToLocation;
+	vector<vector<Mapping>*> maps = { &seedToSoil, &soilToFertilizer, &fertilizerToWater, &waterToLight, &lightToTemp, &tempToHum, &humToLocation };
+	set<ll> seeds;
+	int m = -1;
+	while (!input.eof())
+	{
+		string tr, line;
+		ll seed;
+		getline(input, line);
+		if (line.empty())
+		{
+			m++;
+			getline(input, line); //trash
+			continue;
+		}
+		stringstream sline(line);
+		if (m == -1)
+		{
+			sline >> tr;
+			while (!sline.eof())
+			{
+				sline >> seed;
+				seeds.insert(seed);
+			}
+		}
+		else
+		{
+			ll dest, source, range;
+			sline >> dest >> source >> range;
+			Mapping mapping{ dest, source, range };
+			maps[m]->push_back(mapping);
+		}
+
+	}
+	
+	ll num = LLONG_MAX;
+	for (auto it = seeds.begin(); it != seeds.end(); it++)
+	{
+		ll cur = *it;
+
+		for (int i = 0; i < maps.size(); i++)
+		{
+			cur = FindNum(*maps[i], cur);
+		}
+
+		if (cur < num)
+			num = cur;
+	}
+
+	return num; //199602917
+}
+
+ll Day5_Part2(stringstream& input)
+{
+	vector<Mapping> seedToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemp, tempToHum, humToLocation;
+	vector<vector<Mapping>*> maps = { &seedToSoil, &soilToFertilizer, &fertilizerToWater, &waterToLight, &lightToTemp, &tempToHum, &humToLocation };
+	vector<Seed> seeds;
+	int m = -1;
+	while (!input.eof())
+	{
+		string tr, line;
+		ll seed;
+		getline(input, line);
+		if (line.empty())
+		{
+			m++;
+			getline(input, line); //trash
+			continue;
+		}
+		stringstream sline(line);
+		if (m == -1)
+		{
+			sline >> tr;
+			while (!sline.eof())
+			{
+				ll range;
+				sline >> seed >> range;
+				seeds.push_back(Seed{ seed, range });
+			}
+		}
+		else
+		{
+			ll dest, source, range;
+			sline >> dest >> source >> range;
+			Mapping mapping{ source, dest, range }; //Inverted
+			maps[m]->push_back(mapping);
+		}
+	}
+
+	ll num = 0;
+	
+	while (true)
+	{
+		ll cur = num;
+		for (int i = maps.size()-1; i >= 0; i--)
+		{
+			cur = FindNum(*maps[i], cur);
+		}
+
+		if (isValidSeed(cur, seeds))
+		{
+			break;
+		}
+		num++;
+	}
+
+	return num; //2254686
 }
 
 
