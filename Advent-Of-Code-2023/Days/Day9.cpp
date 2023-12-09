2550aa6582a09bbef628ac4506fbbffc97d9a498
Christian Fikus <christian.fikus@wp.pl>
2023-12-09 13:50:54 +0100

    Day 9

--- Advent-Of-Code-2023/Days/Day9.cpp
+++ Advent-Of-Code-2023/Days/Day9.cpp
@@ -1,13 +1,71 @@
 #include "../general.h"
+#include <algorithm>
+#include <numeric>
 
-string Day9_Part1(stringstream& input)
+void Difference(vector<int> &nums)
 {
-	return "Not implemented yet";
+	vector<int> Diff;
+	for (int i = 1; i < nums.size(); i++)
+		Diff.push_back(nums[i] - nums[i - 1]);
+	if (accumulate(Diff.begin(), Diff.end(), 0) == 0)
+	{
+		Diff.push_back(0);
+		Diff.insert(Diff.begin(), 0);
+	}
+	else
+	{
+		Difference(Diff);
+		nums.push_back(Diff.back() + nums.back());
+		nums.insert(nums.begin(), nums[0] - Diff[0]);
+	}
 }
 
-string Day9_Part2(stringstream& input)
+long long Day9_Part1(stringstream& input)
 {
-	return "Not implemented yet";
+	vector<vector<int>> Numbers;
+	while (!input.eof())
+	{
+		vector<int> LineNum;
+		int num;
+		string line;
+		getline(input, line); 
+		stringstream sline(line);
+		while(sline >> num)
+			LineNum.push_back(num);
+		Numbers.push_back(LineNum);
+	}
+
+	long long sum = 0;
+	for (int i = 0; i < Numbers.size(); i++)
+	{
+		Difference(Numbers[i]);
+		sum += Numbers[i].back();
+	}
+	return sum; //2098530125
+}
+
+long long Day9_Part2(stringstream& input)
+{
+	vector<vector<int>> Numbers;
+	while (!input.eof())
+	{
+		vector<int> LineNum;
+		int num;
+		string line;
+		getline(input, line);
+		stringstream sline(line);
+		while (sline >> num)
+			LineNum.push_back(num);
+		Numbers.push_back(LineNum);
+	}
+
+	long long sum = 0;
+	for (int i = 0; i < Numbers.size(); i++)
+	{
+		Difference(Numbers[i]);
+		sum += Numbers[i][0];
+	}
+	return sum; //1016
 }
 
 
