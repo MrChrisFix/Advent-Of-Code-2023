d4527d4c00aabff6870bbfa89253ecbc130d05f3
Christian Fikus <christian.fikus@wp.pl>
2023-12-11 16:51:17 +0100

    Day 11

--- Advent-Of-Code-2023/Days/Day11.cpp
+++ Advent-Of-Code-2023/Days/Day11.cpp
@@ -1,13 +1,129 @@
 #include "../general.h"
 
-string Day11_Part1(stringstream& input)
+#define ll long long
+
+int Day11_Part1(stringstream& input)
 {
-	return "Not implemented yet";
+	vector<string> universe;
+
+	//Expanding the universe
+	while (!input.eof())
+	{
+		string line;
+		getline(input, line);
+		if (line.find('#') == line.npos)
+			universe.push_back(line);
+		universe.push_back(line);
+	}
+	for (int col = 0; col < universe[0].size(); col++)
+	{
+		bool hasGalaxy = false;
+		for (int row = 0; row < universe.size(); row++)
+		{
+			if (universe[row][col] == '#')
+			{
+				hasGalaxy = true;
+				break;
+			}
+		}
+		if (hasGalaxy)
+			continue;
+
+		for (int row = 0; row < universe.size(); row++)
+		{
+			universe[row].insert(col, ".");
+		}
+		col++;
+
+	}
+
+	vector<pair<int, int>> Galaxies;
+	int y = 0;
+	for (string line : universe)
+	{
+		for (int x = 0; x < line.size(); x++)
+			if (line[x] == '#')
+				Galaxies.push_back(pair<int, int>(x, y));
+		y++;
+	}
+
+	int sum = 0;
+
+	for (int i = 0; i < Galaxies.size(); i++)
+	{
+		for (int j = i + 1; j < Galaxies.size(); j++)
+		{
+			int dist = abs(Galaxies[i].first - Galaxies[j].first) + abs(Galaxies[i].second - Galaxies[j].second);
+			sum += dist;
+		}
+	}
+
+	return sum;
 }
 
-string Day11_Part2(stringstream& input)
+ll Day11_Part2(stringstream& input)
 {
-	return "Not implemented yet";
+	vector<string> universe;
+	vector<ll> Ys, Xs;
+	const int EXP = 1000000;
+
+	//Expanding the universe
+	ll Y = 0;
+	while (!input.eof())
+	{
+		string line;
+		getline(input, line);
+		if (line.find('#') == line.npos)
+			Y += EXP;
+		else
+			Y++;
+		Ys.push_back(Y);
+		universe.push_back(line);
+	}
+	ll X = 0;
+	for (int col = 0; col < universe[0].size(); col++)
+	{
+		bool hasGalaxy = false;
+		for (int row = 0; row < universe.size(); row++)
+		{
+			if (universe[row][col] == '#')
+			{
+				hasGalaxy = true;
+				break;
+			}
+		}
+		if (hasGalaxy)
+			X++;
+		else
+			X += EXP;
+		Xs.push_back(X);
+	}
+
+	//Finding galaxies
+	vector<pair<ll, ll>> Galaxies;
+	int y = 0;
+	for (string line : universe)
+	{
+		for (int x = 0; x < line.size(); x++)
+			if (line[x] == '#')
+			{
+				Galaxies.push_back(pair<ll, ll>(Xs[x], Ys[y]));
+			}
+		y++;
+	}
+
+	//Calculating the distances
+	ll sum = 0;
+	for (int i = 0; i < Galaxies.size(); i++)
+	{
+		for (int j = i + 1; j < Galaxies.size(); j++)
+		{
+			int dist = abs(Galaxies[i].first - Galaxies[j].first) + abs(Galaxies[i].second - Galaxies[j].second);
+			sum += dist;
+		}
+	}
+
+	return sum;
 }
 
 
